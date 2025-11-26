b63f406e7eaa461662b538d96a24edebf52033a1
Mohana <your-email@example.com>
2025-11-26 11:30:03 +0530

    Task3

---
+++ Mohana.py
@@ -0,0 +1,9 @@
+print("hello mohana")
+print("welcome to fullstack development")
+print("this is day 2 of fullstack development")
+print("we are learning github and vs code")
+n=int(input("enter a number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print("factorial of",n,"is",fact)
